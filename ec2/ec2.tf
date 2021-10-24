d02105f81f09745126ff10ad3d8920e0db60f746
Fernando de Araujo Marques <fernanndomarques@terra.com.br>
2021-10-24 13:34:19 -0300

    new files

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -30,19 +30,6 @@ resource "null_resource" "remote-exec" {
     inline = [
       "sudo apt-get update -y",
       "sudo apt-get upgrade -y",
-      "curl https://releases.rancher.com/install-docker/20.10.sh | sh",
-      "sudo apt-get update -y",
-      "sudo apt-get install -y apt-transport-https ca-certificates curl",
-      "sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg"
-      "echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list"",
-      "sudo apt-get update",
-      "sudo apt-get install -y kubelet kubeadm kubectl",
-      "sudo apt-mark hold kubelet kubeadm kubectl",
-      "curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl",
-      "sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl",
-      "chmod +x kubectl",
-      "mkdir -p ~/.local/bin/kubectl",
-      "mv ./kubectl ~/.local/bin/kubectl",
-    ]
+      ]
   }
 }
--- ec2/variables.tf
+++ ec2/variables.tf
@@ -19,5 +19,5 @@ variable "aws_instance" {
 
 variable "aws_instance_count" {
   type    = string
-  default = "2"
+  default = "3"
 }
